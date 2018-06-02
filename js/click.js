c64cdc6b4c0731c29cbe7ec78391d251baded314
Tyler Yep <tyep@stanford.edu>
2018-06-02 13:41:45 -0700

    fix rotation issues

--- js/click.js
+++ js/click.js
@@ -30,8 +30,11 @@ $(document).ready(function() {
     text.setAttribute('value', 'Good job!');
   });
   text.setAttribute('value', 'Focus the cursor on me!');
-  position.x += 1;
-  text.setAttribute('position', position);
+  var position2 = marker.object3D.getWorldPosition();
+  position2.x = -4;
+  position2.y = 1.5;
+  position2.z = -3;
+  text.setAttribute('position', position2);
 
   scene.appendChild(box);
   scene.appendChild(text);
